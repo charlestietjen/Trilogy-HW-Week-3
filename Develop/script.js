5ee435084794c4ca7cdbde6fd42a3d7cca36de69
Charles T <tietjen.charles@gmail.com>
2021-11-30 21:29:00 -0500

    initial commit but also probably the final commit

---
+++ Develop/script.js
@@ -0,0 +1,60 @@
+// Assignment Code
+
+//global vars
+var generateBtn = document.querySelector("#generate");
+
+var avChars = []
+
+//global functions
+function genCharSet(start, end) {
+  var a = [], i = start.charCodeAt(0), j = end.charCodeAt(0);
+  for (; i <= j; ++i) {
+      a.push(String.fromCharCode(i));
+  }
+  return a;
+}
+
+function generatePassword() {
+  var a = [];
+  var pw = ""
+  var length = window.prompt("How many characters between 8 and 128?")
+  if (length < 8 || length > 128) {
+    window.alert("Invalid length selection, please try again.")
+  }
+  else {
+    var addLC = window.confirm("Would you like lower case characters?")
+      if (addLC) {
+        a = a.concat(genCharSet("a", "z"))
+      }
+    var addUC = window.confirm("Would you like upper case characters?")
+      if (addUC) {
+        a = a.concat(genCharSet("A", "Z"))
+      }
+    var addSC = window.confirm("Would you like special characters?")
+      if (addSC) {
+        a = a.concat(genCharSet("!", "/"))
+      }
+    var addNum = window.confirm("Would you like numbers?")
+      if (addNum) {
+        a = a.concat(genCharSet("0", "9"))
+      }
+    var i = 0
+    for (; i < length; i++){
+      var b = Math.floor(Math.random() * a.length);
+      pw += a[b];
+    }
+  return pw;
+  }
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
